dfcd48b89b2a5adaab8ed3cd23f4ed6314d64ae7
CG360 <16578982+CloudG360@users.noreply.github.com>
2023-02-09 19:58:46 +0000

    Fixed crash with recipe remainders for upgrades (Issue #5)

--- src/main/java/com/yyon/grapplinghook/item/upgrade/BaseUpgradeItem.java
+++ src/main/java/com/yyon/grapplinghook/item/upgrade/BaseUpgradeItem.java
@@ -1,44 +1,21 @@
 package com.yyon.grapplinghook.item.upgrade;
 
 import com.yyon.grapplinghook.util.GrappleCustomization;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.world.item.AirItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class BaseUpgradeItem extends Item {
 	public GrappleCustomization.UpgradeCategories category = null;
-	boolean craftingRemaining = false;
+
+	public BaseUpgradeItem() {
+		this(64, null);
+	}
 
 	public BaseUpgradeItem(int maxStackSize, GrappleCustomization.UpgradeCategories theCategory) {
 		super(new Item.Properties().stacksTo(maxStackSize));
 		
 		this.category = theCategory;
-		
-		if (theCategory != null) {
-			this.setCraftingRemainingItem();
-		}
-	}
-	
-	public void setCraftingRemainingItem() {
-		craftingRemaining = true;
-	}
-
-	//TODO: @Override
-	public ItemStack getCraftingRemainingItem(ItemStack itemStack)
-    {
-        if (!this.craftingRemaining)
-        {
-            return ItemStack.EMPTY;
-        }
-        return new ItemStack(this);
-    }
-	
-	@Override
-	public boolean hasCraftingRemainingItem() {
-		return this.craftingRemaining;
-	}
-
-
-	public BaseUpgradeItem() {
-		this(64, null);
 	}
 }
