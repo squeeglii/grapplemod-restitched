815c9a7781bfe875e430829325bf6dc496ec0810
CG360 <16578982+CloudG360@users.noreply.github.com>
2023-05-12 20:13:53 +0100

    Fixed camera not tilting when wall-running

--- src/main/java/com/yyon/grapplinghook/mixin/client/CameraSetupHookMixin.java
+++ src/main/java/com/yyon/grapplinghook/mixin/client/CameraSetupHookMixin.java
@@ -1,6 +1,8 @@
 package com.yyon.grapplinghook.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import com.yyon.grapplinghook.GrappleMod;
 import com.yyon.grapplinghook.client.ClientPhysicsContextTracker;
 import com.yyon.grapplinghook.config.GrappleModLegacyConfig;
 import com.yyon.grapplinghook.physics.context.AirFrictionPhysicsContext;
@@ -10,6 +12,7 @@ import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.world.entity.player.Player;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,11 +23,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class CameraSetupHookMixin {
 
-    @Final
-    @Shadow
-    private Camera mainCamera;
+    private float currentCameraTilt = 0;
 
-    protected float currentCameraTilt = 0;
     @Inject(method = "renderLevel(FJLcom/mojang/blaze3d/vertex/PoseStack;)V",
             at = @At(
                     value = "INVOKE",
@@ -33,22 +33,20 @@ public class CameraSetupHookMixin {
             ))
     public void postCameraSetup(float partialTicks, long finishTimeNano, PoseStack matrixStack, CallbackInfo ci) {
         Player player = Minecraft.getInstance().player;
-        if (!Minecraft.getInstance().isRunning() || player == null) {
-            return;
-        }
+        if (!Minecraft.getInstance().isRunning() || player == null) return;
 
         int id = player.getId();
         int targetCameraTilt = 0;
+
         if (ClientPhysicsContextTracker.controllers.containsKey(id)) {
             GrapplingHookPhysicsContext controller = ClientPhysicsContextTracker.controllers.get(id);
-            if (controller instanceof AirFrictionPhysicsContext afcontroller) {
-                if (afcontroller.wasWallrunning) {
-                    Vec walldirection = afcontroller.getWallDirection();
-                    if (walldirection != null) {
-                        Vec lookdirection = Vec.lookVec(player);
-                        int dir = lookdirection.cross(walldirection).y > 0 ? 1 : -1;
-                        targetCameraTilt = dir;
-                    }
+
+            if (controller instanceof AirFrictionPhysicsContext physicsContext && physicsContext.wasWallrunning) {
+
+                Vec wallDirection = physicsContext.getWallDirection();
+                if (wallDirection != null) {
+                    Vec lookDirection = Vec.lookVec(player);
+                    targetCameraTilt = lookDirection.cross(wallDirection).y > 0 ? 1 : -1;
                 }
             }
         }
@@ -57,20 +55,20 @@ public class CameraSetupHookMixin {
             float cameraDiff = targetCameraTilt - currentCameraTilt;
             if (cameraDiff != 0) {
                 float anim_s = GrappleModLegacyConfig.getClientConf().camera.wallrun_camera_animation_s;
-                float speed = (anim_s == 0) ? 9999 :  1.0f / (anim_s * 20.0f);
-                if (speed > Math.abs(cameraDiff)) {
-                    currentCameraTilt = targetCameraTilt;
-                } else {
-                    currentCameraTilt += speed * (cameraDiff > 0 ? 1 : -1);
-                }
+                float speed = (anim_s == 0)
+                        ? 9999
+                        :  1.0f / (anim_s * 20.0f);
+
+                currentCameraTilt = speed > Math.abs(cameraDiff)
+                        ? targetCameraTilt
+                        : currentCameraTilt + speed * (cameraDiff > 0 ? 1 : -1);
             }
         }
 
-        if (currentCameraTilt != 0) {
-            // Observing the forge hook, roll just isn't used.
-            // Fix this.
-            //mainCamera.(0 + currentCameraTilt*GrappleConfig.getClientConf().camera.wallrun_camera_tilt_degrees);
-        }
+        if (this.currentCameraTilt == 0) return;
+
+        float angle = this.currentCameraTilt * GrappleModLegacyConfig.getClientConf().camera.wallrun_camera_tilt_degrees;
+        matrixStack.mulPose(Axis.ZP.rotationDegrees(angle));
     }
 
 }
