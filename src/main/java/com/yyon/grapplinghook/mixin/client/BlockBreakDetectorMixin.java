9b7fb59c4f750e3496adbd45c3575e8b4d52c7c5
Will <16578982+CloudG360@users.noreply.github.com>
2023-08-26 22:11:37 +0100

    Fixed clientside mixin targeting the wrong class?

--- src/main/java/com/yyon/grapplinghook/mixin/client/BlockBreakDetectorMixin.java
+++ src/main/java/com/yyon/grapplinghook/mixin/client/BlockBreakDetectorMixin.java
@@ -3,20 +3,19 @@ package com.yyon.grapplinghook.mixin.client;
 import com.yyon.grapplinghook.client.physics.ClientPhysicsControllerTracker;
 import com.yyon.grapplinghook.client.GrappleModClient;
 import com.yyon.grapplinghook.client.physics.context.GrapplingHookPhysicsController;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerPlayerGameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerPlayerGameMode.class)
+@Mixin(MultiPlayerGameMode.class)
 public class BlockBreakDetectorMixin {
 
-    // Was in client events but uses a server class????
-
-    @Inject(method = "destroyBlock(Lnet/minecraft/core/BlockPos;)Z", at = @At(value = "HEAD", shift = At.Shift.BY, by = 1))
+    @Inject(method = "destroyBlock(Lnet/minecraft/core/BlockPos;)Z", at = @At("RETURN"))
     public void handleBlockBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if(!cir.getReturnValue()) return;
         if (pos == null) return;
 
         ClientPhysicsControllerTracker physManager = GrappleModClient.get().getClientControllerManager();
