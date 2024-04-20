25c4a57c67a032e2659ffed39aeb5f6209b8a872
Will Scully <16578982+squeeglii@users.noreply.github.com>
2024-04-20 01:47:24 +0100

    Fixed gradle for GlidersCompatModule

--- Compat/Gliders/src/main/java/me/cg360/mod/grapplemod/compat/gliders/GlidersCompatEntrypoint.java
+++ Compat/Gliders/src/main/java/me/cg360/mod/grapplemod/compat/gliders/GlidersCompatEntrypoint.java
@@ -3,21 +3,31 @@ package me.cg360.mod.grapplemod.compat.gliders;
 import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
 
 public class GlidersCompatEntrypoint implements ModInitializer {
 
+    private static boolean hasLoaded = false;
+
     @Override
     public void onInitialize() {
-        if(!FabricLoader.getInstance().isModLoaded("vc_gliders"))
+        Logger logger = LogUtils.getLogger();
+        if(!FabricLoader.getInstance().isModLoaded("vc_gliders")) {
+            logger.info("'vc_gliders' is not installed. Disabling compatibility");
             return;
+        }
+
+        logger.info("'vc_gliders' is installed. Enabling compatibility");
 
         try {
-            Class.forName("me.cg360.mod.grapplemod.compat.gliders.GlidersCompatEntrypoint")
+            Class.forName("me.cg360.mod.grapplemod.compat.gliders.GlidersCompatModule")
                     .getConstructor()
                     .newInstance();
 
+            hasLoaded = true;
+
         } catch (Exception err) {
-            LogUtils.getLogger().error(err.getMessage());
+            logger.error(err.getMessage());
         }
     }
 
--- Compat/Gliders/src/main/java/me/cg360/mod/grapplemod/compat/gliders/GlidersCompatModule.java
+++ Compat/Gliders/src/main/java/me/cg360/mod/grapplemod/compat/gliders/GlidersCompatModule.java
@@ -6,8 +6,5 @@ public class GlidersCompatModule {
 
     public GlidersCompatModule() {
         LogUtils.getLogger().info("Enabled Gliders Compatibility Module");
-
-        //TODO: Add module contents - this needs a 1.19.2 backport of GrappleMod v2 to be
-        // completed before work can be done.
     }
 }
